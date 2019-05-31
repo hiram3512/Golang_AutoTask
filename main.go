d2e045be559ceb59bac0ae17357c80a447067d32
Hiram <hiramtan@live.com>
2019-05-31 16:39:45 +0800

    finish update and load logic

--- main.go
+++ main.go
@@ -29,18 +29,17 @@ func main() {
 	unityPath = strings.Replace(text, "\r\n", "", -1) + "/Unity.exe"
 	fmt.Printf("输入项目路径:")
 	text, _ = reader.ReadString('\n')
-	projectPath = text
+	projectPath = strings.Replace(text, "\r\n", "", -1)
 	fmt.Println("--------------------------------------\n开始执行定时任务\n每天凌晨1点自动更新svn\n每天凌晨2点自动载入unity\n--------------------------------------")
 
 	c := cron.New()
-	c.AddFunc("0 35 14 * * ?", func() {
+	c.AddFunc("0 0 1 * * ?", func() {
 		//强制关闭unity
 		taskkill := exec.Command("taskkill", "/f", "/im", "unity.exe")
 		taskkill.Run()
 
 		fmt.Println(time.Now(), "自动更新Svn")
 		out := bytes.NewBuffer(nil)
-		projectPath = "D:/MySvn/QinUI"
 		cmd := exec.Command("svn", "update", projectPath)
 		cmd.Stdout = out
 		cmd.Run()
@@ -52,7 +51,7 @@ func main() {
 	c.Start()
 
 	c2 := cron.New()
-	c2.AddFunc("0 40 14 * * ?", func() {
+	c2.AddFunc("0 0 2 * * ?", func() {
 		fmt.Println(time.Now(), "自动载入Unity")
 		out := bytes.NewBuffer(nil)
 		cmd := exec.Command(unityPath, "-projectPath", projectPath)
