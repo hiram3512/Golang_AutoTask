fff66198c53f3b722ceaf32d39eda7d58ab14a8c
Hiram <hiramtan@live.com>
2019-05-31 11:27:56 +0800

    add svn and unity command

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 /*
 每天凌晨1点自动更新svn
+每天凌晨2点自动载入unity
 */
 
 package main
@@ -14,17 +15,20 @@ import (
 	"github.com/robfig/cron"
 )
 
-func main() {
-	// ticker := time.NewTicker(time.Minute * 10)
-	// for _ = range ticker.C {
-	// 	fmt.Println(time.Now(), "更新Svn")
-	// 	cmd := exec.Command("svn", "update", "D:/MySvn/QinUI")
-	// 	cmd.Run()
-	// }
+var (
+	unityPath, projectPath string
+)
 
+func main() {
+	fmt.Printf("输入Unity安装路径:")
+	fmt.Scanln(&unityPath)
+	unityPath = unityPath + "/Unity.exe"
+	fmt.Printf("输入项目路径:")
+	fmt.Scanln(&projectPath)
+	fmt.Println("开始执行定时任务")
+	fmt.Println("每天凌晨1点自动更新svn\n每天凌晨2点自动载入unity")
 	c := cron.New()
-	//c.AddFunc("0 0 1 * * ?", func() {
-	c.AddFunc("0 0 21 27 * ?", func() {
+	c.AddFunc("0 20 11 * * ?", func() {
 		fmt.Println(time.Now(), "自动更新Svn")
 		out := bytes.NewBuffer(nil)
 		cmd := exec.Command("svn", "update", "D:/MySvn/QinUI")
@@ -36,4 +40,20 @@ func main() {
 		fmt.Println(goStr)
 	})
 	c.Start()
+
+	c2 := cron.New()
+	c2.AddFunc("0 22 11 * * ?", func() {
+		fmt.Println(time.Now(), "自动载入Unity")
+		out := bytes.NewBuffer(nil)
+		cmd := exec.Command(unityPath, "-projectPath", projectPath)
+		cmd.Stdout = out
+		cmd.Run()
+
+		enc := mahonia.NewDecoder("gb18030")
+		goStr := enc.ConvertString(out.String())
+		fmt.Println(goStr)
+	})
+	c2.Start()
+
+	fmt.Scanln()
 }
